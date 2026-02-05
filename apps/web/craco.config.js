2b5672972c4bcf7d722e01af72b5514b460b1257
jungin <changju@bu.edu>
2026-02-04 22:26:38 -0600

    allow cracro use more rams

--- apps/web/craco.config.js
+++ apps/web/craco.config.js
@@ -5,5 +5,28 @@ module.exports = {
         alias: {
             '@': path.resolve(__dirname, 'src'),
         },
+        configure: (webpackConfig, { env, paths }) => {
+            if (env === 'production') {
+                // 1. Disable Source Maps to save massive amounts of RAM
+                webpackConfig.devtool = false;
+
+                // 2. Remove the heavy Type-Checking plugin during the build
+                // This offloads memory but remember to run `tsc` separately!
+                webpackConfig.plugins = webpackConfig.plugins.filter(
+                    (plugin) => plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin'
+                );
+
+                // 3. (Optional) Optimize Terser (the minifier)
+                // If you still crash, we can disable minification to test, 
+                // though it's not recommended for production.
+                const terserPlugin = webpackConfig.optimization.minimizer.find(
+                    (m) => m.constructor.name === 'TerserPlugin'
+                );
+                if (terserPlugin) {
+                    terserPlugin.options.parallel = true; // Use multi-core
+                }
+            }
+            return webpackConfig;
+        },
     },
 };
